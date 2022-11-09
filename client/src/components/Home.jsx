7fb621f9318bd91419ec596f6a24680bb5236473
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-11-09 23:57:36 +0100

    css

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
-import './Home.css'
+import './home.css'
 
 const Home = () => {
   return (
-    <div>
+    <div className='ana'>
         <div className="login-card">
             <h2>Login</h2>
             <h3>Enter Your Username</h3>
             <form className='login-form'>
                 <input type="text" placeholder='Username' />
                 <input type="password" placeholder='Password' />
-                <a href="#">
-                    Forget your password?
-                </a>
+              
                 <button>Login</button>
             </form>
         </div>

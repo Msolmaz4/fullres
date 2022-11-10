bdeb5a82349e559d43b8535f62725244e26589c1
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-11-11 00:11:26 +0100

    add

---
+++ client/src/components/Adbutton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './button.css'
+
+const Adbutton = () => {
+  return (
+    <div>
+        <button>ADD</button>
+    </div>
+  )
+}
+
+export default Adbutton
--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Adbutton from './Adbutton'
 import Search from './Search'
 
 const Cart = () => {
   return (
     <div>
         <Search/>
+        <Adbutton/>
     </div>
   )
 }

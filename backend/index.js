f40ef1e3c6242f77b51dd951a963bd2c10eabfc8
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-11-12 23:17:56 +0100

    baglanti

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,9 @@ const userRouter = require('./router/userRouter.js')
 const dotenv =require('dotenv')
 dotenv.config( { path:'.env'})
 const productRouter = require('./router/productRouter.js')
+//burada dikkat et hata veroyor yoksa backend on yuz
+const cors = require('cors')
+app.use(cors())
 
 app.use(express.urlencoded({ extended :false}))
 app.use(express.json())
